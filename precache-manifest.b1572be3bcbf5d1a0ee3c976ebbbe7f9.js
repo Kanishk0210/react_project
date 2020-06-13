4e887a7e8fc5ca3548fc10a7e36eb9ec1dfa94c3
kanishk0210 <kanishkchoudhary02@gmail.com>
2020-06-13 11:35:50 +0530

    Updates

---
+++ precache-manifest.b1572be3bcbf5d1a0ee3c976ebbbe7f9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7af92e8084fda18df5566a2a3a996b4",
+    "url": "/RistoranteconFusion/index.html"
+  },
+  {
+    "revision": "2e7ba897500f39773996",
+    "url": "/RistoranteconFusion/static/css/2.ee780f76.chunk.css"
+  },
+  {
+    "revision": "eff884dcd1021ab3e6bf",
+    "url": "/RistoranteconFusion/static/css/main.bc2a2813.chunk.css"
+  },
+  {
+    "revision": "2e7ba897500f39773996",
+    "url": "/RistoranteconFusion/static/js/2.5056ffe1.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/RistoranteconFusion/static/js/2.5056ffe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff884dcd1021ab3e6bf",
+    "url": "/RistoranteconFusion/static/js/main.524d2046.chunk.js"
+  },
+  {
+    "revision": "3c2ca62a1da28134bd13",
+    "url": "/RistoranteconFusion/static/js/runtime-main.3347d58d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/RistoranteconFusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/RistoranteconFusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/RistoranteconFusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/RistoranteconFusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/RistoranteconFusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Kanishk0210/react_project.git/precache-manifest.247e9c5edc1d4a48d12b77b48b573187.js"
+  "/RistoranteconFusion/precache-manifest.b1572be3bcbf5d1a0ee3c976ebbbe7f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Kanishk0210/react_project.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RistoranteconFusion/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
